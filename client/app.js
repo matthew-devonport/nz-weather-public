d9b5649a6d1d533e04df7d50c5e0b47dfaf4eb43
Student <student@pop-os.localdomain>
2019-08-30 09:22:24 +1200

    finished daily apis

--- client/app.js
+++ client/app.js
@@ -27,9 +27,16 @@ let html = `<h1>${weatherData.name} - ${weatherData.sys.country}</h1>
             <p>Max Temp: ${weatherData.main.temp_max}<p>
             <p>Min Temp: ${weatherData.main.temp_min}<p>
             <p>Humidity: ${weatherData.main.humidity}<p>
-            <h2>Other Information</h2>
+            <p>Pressure: ${weatherData.main.pressure}<p><br>
+            <h2>Weather Description</h2>
+            <p>Weather Description: ${weatherData.weather[0].description}<p><br>
+            <h2>Sunset / Sunrise</h2>
             <p>Sunrise: ${moment.unix(weatherData.sys.sunrise)}<p>
-            <p>Sunset: ${moment.unix(weatherData.sys.sunset)}<p>
+            <p>Sunset: ${moment.unix(weatherData.sys.sunset)}<p><br>
+            <h2>Wind</h2>
+            <p>Wind Speed: ${weatherData.wind.speed} mps<p>
+            <p>Wind Direction: ${weatherData.wind.deg}<p>
+
 
 `
 document.getElementById('weather-data').innerHTML = html
