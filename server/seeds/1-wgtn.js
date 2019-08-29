f81fa978adbe8f19ee87b222282ad77f6e17256e
Alex-Abernethy <Alexander.Abernethy@gmail.com>
2019-08-30 09:17:54 +1200

    updated db tables to weatherDescription

--- server/seeds/1-wgtn.js
+++ server/seeds/1-wgtn.js
@@ -1,5 +1,5 @@
 exports.seed = function (knex, Promise) {
   return knex('wgtn').insert([
-    { id: 1, country: 'NZ', city: 'Wellington', currentTemp: 13.52, maxTemp: 15, minTemp: 12.22, humidity: 62, pressure: 1013, cloudDescription: 'scattered clouds', windSpeed: 9.3, windDirection: 360, sunrise: 1567057953, sunset: 1567057953},
+    { id: 1, country: 'NZ', city: 'Wellington', currentTemp: 13.52, maxTemp: 15, minTemp: 12.22, humidity: 62, pressure: 1013, weatherDescription: 'scattered clouds', windSpeed: 9.3, windDirection: 360, sunrise: 1567057953, sunset: 1567057953},
   ])
 }
